74aba9a870bcfc6aa1c3ab8c16d374f28f304285
James <Jamesronaldoperez@gmail.com>
2023-10-15 23:27:28 -0400

    Tiene algunos problemas, al momento de filtrar las tareas ya realizadas

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -63,3 +63,45 @@ function MarcarHecho(id) {
     });
 
 }
+
+function FiltrarHecho(){
+  const cardContainer = document.getElementById("tareas_realizadas");
+
+  // Realizar la solicitud fetch
+  fetch('/Tasks/true')
+    .then(response => {
+      // Verificar si la solicitud fue exitosa (código de respuesta 200)
+      if (!response.ok) {
+        throw new Error('No se pudo obtener la información');
+      }
+      // Convertir la respuesta a formato JSON
+      return response.json();
+    })
+    .then(data => {
+      // Iterar a través de los datos del backend y crear tarjetas para cada tarea
+      data.forEach(task => {
+        // Crear una tarjeta HTML para la tarea
+        const card = document.createElement('div');
+        card.className = 'col-sm-5 p-1 m-2 mb-sm-0';
+        card.innerHTML = `
+            <div class="card" ">
+              <div class="card-body">
+                <h5 class="card-title">${task.titulo}</h5>
+                <p class="card-text">${task.descripcion}</p>
+                <p class="card-text">${task.eta}<br>
+               <strong>Realizada;${task.finished}</strong></p>
+                <a href="index.html" class="btn btn-primary" onclick="MarcarHecho(${task.id})">Tarea hecha</a>
+                <a href="index.html" class="btn btn-primary" onclick="EliminarTarea(${task.id})">Borrar Tarea</a>
+              </div>
+            </div>
+          `;
+        // Agregar la tarjeta al contenedor
+        cardContainer.appendChild(card);
+      });
+    })
+    .catch(error => {
+      // Manejar errores de solicitud o respuesta
+      console.error('Hubo un error:', error);
+    });
+
+}
